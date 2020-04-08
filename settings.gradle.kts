03c93bceb0232a2f42d8e3df8908eb649e3234e6
Danilo Pianini <danilo.pianini@unibo.it>
2020-04-08 17:38:09 +0200

    Switch to refreshVersions, temporarily disable Spotbugs due to spotbugs/spotbugs-gradle-plugin#228

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
+import de.fayard.dependencies.bootstrapRefreshVersionsAndDependencies
+
 rootProject.name = "thread-inheritable-resource-loader"
+
+buildscript {
+    repositories { gradlePluginPortal() }
+    dependencies.classpath("de.fayard:dependencies:+")
+}
+
+bootstrapRefreshVersionsAndDependencies()
